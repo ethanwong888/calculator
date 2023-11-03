8b9e611b57eb97bf304a183836b5fdfc51e8c759
Ethan Wong <ethanwong@g.ucla.edu>
2023-11-02 22:37:50 -0700

    fixed calculator functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # calculator
 
 calculator created using js, html, and css
+
+[Live Demo](https://raw.githack.com/ethanwong888/calculator/main/index.html)
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let displayValue = "";
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
 
 function updateDisplay() {
 	document.getElementById("display").value = displayValue;
@@ -11,6 +14,9 @@ function appendToDisplay(value) {
 
 function clearDisplay() {
 	displayValue = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
 	updateDisplay();
 }
 
@@ -19,12 +25,50 @@ function deleteCharacter() {
 	updateDisplay();
 }
 
-function calculateResult() {
-	try {
-			displayValue = eval(displayValue);
-			updateDisplay();
-	} catch (error) {
-			displayValue = "Error";
-			updateDisplay();
-	}
+function setOperation(operator) {
+  // ex: 5*8+3 -> once the + is encountered, want to evaluate the 5*8 first
+  if (currentOperation !== null) {
+    evaluateExpression();
+  }
+  firstOperand = displayValue;
+  currentOperation = operator;
+  displayValue = "";
+}
+
+function roundResult(num) {
+  return Math.round(num * 100000) / 100000;
+}
+
+function evaluateExpression() {
+  if (currentOperation === null){
+    return;
+  }
+  
+  secondOperand = displayValue;
+  if (currentOperation === '÷' && secondOperand === '0') {
+    alert("You can't divide by 0!");
+    return;
+  }
+
+  displayValue = roundResult(operate(currentOperation, firstOperand, secondOperand));
+  updateDisplay();
+  currentOperation = null;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case '+':
+      return (a+b);
+    case '-':
+      return (a-b);
+    case '×':
+      return (a*b);
+    case '÷':
+      if (b === 0) return null
+      else return (a/b);
+    default:
+      return null
+  }
 }
